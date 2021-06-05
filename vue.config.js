9ad53f074e8a60fc744209b11e2f4c6e254909f1
Maks Voloshyn <maks.m.voloshyn@gmail.com>
2021-06-05 12:56:01 +0200

    Always look for VUE_APP_API_KEY;

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/currency-converter/' : '/',
+    chainWebpack: config => {
+        config.plugin('define').tap(([options]) => {
+            return [
+                {
+                    ...options,
+                    'process.env': {
+                        ...options['process.env'],
+                        VUE_APP_API_KEY: JSON.stringify(process.env.VUE_APP_API_KEY),
+                    },
+                },
+            ];
+        });
+    },
 };
