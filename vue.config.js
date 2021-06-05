2e667d17d65ee8b3de12ebe102cc6dfa10c63c52
Maks Voloshyn <maks.m.voloshyn@gmail.com>
2021-06-05 13:21:44 +0200

    Try another env;

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/currency-converter/' : '/',
     chainWebpack: config => {
         config.plugin('define').tap(([options]) => {
             return [
